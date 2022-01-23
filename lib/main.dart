53b4912626655a3d567095a6ca6983411d9b55dd
kl <datesaver1234@gmail.com>
2022-01-23 21:27:12 +0900

    fix material app

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
       home: TaskScreen(),
     );
