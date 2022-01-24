3e1e39e4a00c77123c0e1d5dc9bfb93a223d6a9a
kl <datesaver1234@gmail.com>
2022-01-25 03:15:22 +0900

    google login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,9 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LoginScreen extends HookConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  Future<void> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn(scopes: [
+      'email',
+    ]).signIn();
+    final googleAuth = await googleUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    // サインインしたら、UserCredentialを返す
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    return;
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -19,8 +34,8 @@ class LoginScreen extends HookConsumerWidget {
               primary: Colors.orange,
               onPrimary: Colors.white,
             ),
-            onPressed: () {
-              FirebaseAuth.instance.signInAnonymously();
+            onPressed: () async {
+              await signInWithGoogle();
             },
             child: const Text('ログイン'),
           ),
--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:test_interval/data/providers/task_list_controller.dart';
@@ -24,7 +25,10 @@ class SettingsScreen extends HookConsumerWidget {
                 primary: Colors.orange,
                 onPrimary: Colors.white,
               ),
-              onPressed: () => FirebaseAuth.instance.signOut(),
+              onPressed: () {
+                GoogleSignIn.standard().signOut();
+                FirebaseAuth.instance.signOut();
+              },
               child: const Text('ログアウト'),
             ),
           ],
